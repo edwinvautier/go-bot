837a9473882a588f77e1f800658df54ce3c876f8
Edwin Vautier <edwin.vautier@hetic.net>
2020-11-13 10:55:01 +0100

    fix: fixed potential error in youtube command.

--- commands/youtube_query.go
+++ commands/youtube_query.go
@@ -8,13 +8,18 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+// QueryYoutubeVideoCommand is the struct for our youtube query command
 type QueryYoutubeVideoCommand struct {
 	analysis *wit.Analysis
 	session  *discordgo.Session
 	message  *discordgo.MessageCreate
 }
 
+// Execute function of the youtube query command
 func (command QueryYoutubeVideoCommand) Execute() error {
+	if len(command.analysis.Music) < 1 {
+		return errors.New("Missing entity music in analysis")
+	}
 	music := command.analysis.Music[0].Value
 	if len(music) == 0 {
 		return errors.New("No music value specified")
@@ -29,7 +34,7 @@ func (command QueryYoutubeVideoCommand) Execute() error {
 		_, err := command.session.ChannelMessageSend(command.message.ChannelID, "https://youtu.be/"+v.Id)
 		if err != nil {
 			log.Error("could not send message to channelId: ", command.message.ChannelID)
-			return errors.New("Could not send message.")
+			return errors.New("Could not send message")
 		}
 	}
 	return nil
